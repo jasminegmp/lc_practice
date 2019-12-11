18053c4cd5161eee4655c684fbae36af266fc599
Jasmine <jasmine.kim.18@gmail.com>
2019-12-11 14:11:17 -0800

    Cleaned up comments of problem 125

--- problem_125.js
+++ problem_125.js
@@ -4,41 +4,38 @@
  */
 function isPalindrome(s) {
     let stack = [];
-    
     let start_idx;
     let start_half_end;
 
-    s = s.toLowerCase().replace(/[^a-z0-9]/g, ''); //replaces all non alphabet characters with ''
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, ''); //replaces all non alphanumeric characters with ''
+    
+    // find where the halfway of string char is
     let str_length = s.length;
     start_half_end = Math.floor(str_length / 2);
 
-    // if odd string, remember to start after the halfway character
+    // if string is of odd length, remember to start after the halfway character
     if (str_length % 2){
-        console.log("odd!", start_half_end)
         start_idx = start_half_end + 1;
     }
-    else{console.log("even!", start_half_end)
-    start_idx = start_half_end;
+    else{
+        start_idx = start_half_end;
     }
 
+    // place items into stack up until halfway char
+    // then at halfway char, start popping off the stack if the chars match
     for(let i = 0; i < str_length; i++){
      if (i < start_half_end){
         stack.push(s[i]);
      }
-     console.log( i, start_idx)
      if (i >= start_idx){
         if(stack.pop() !== s[i]){
             return false;
         }   
      }
-     console.log(stack)
     }
-    
-    console.log(stack);
-    console.log(s);
 
+    // if items left in stack, not a palindrome
     if (stack.length > 0){
-        console.log('here')
         return false;
     }
     return true
