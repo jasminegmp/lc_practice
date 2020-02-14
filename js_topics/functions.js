346f5352a798597832c4f15fcdb672faf32059a7
Jasmine <jasmine.kim.18@gmail.com>
2020-02-13 16:04:37 -0800

    JS topics

---
+++ js_topics/functions.js
@@ -0,0 +1,12 @@
+// function declaration
+function add(x,y){
+    return x+ y;
+}
+
+// function expression
+var add = function(x,y){
+    return x+y;
+}
+
+// arrow function using function expression
+var add = (x,y) =>{x + y}
