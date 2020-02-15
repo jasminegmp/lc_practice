cf11fb828db8a8cbda4c80fabb1fb26752a39999
Jasmine <jasmine.kim.18@gmail.com>
2020-02-15 06:11:07 -0800

    This binding rules

--- js_topics/this_binding.js
+++ js_topics/this_binding.js
@@ -1,3 +1,6 @@
+/*
+Implicit Binding
+*/
 var sayNameFunction = function(obj){
     obj.sayName = function(){
         console.log(this.name);
@@ -18,3 +21,54 @@ sayNameFunction(you);
 
 me.sayName()
 you.sayName()
+
+/*
+Explicit Binding - explicitly state this
+call - invoke function
+apply - invoke function arg as array
+bind - returns you a new function instead of invoking function
+*/
+
+let sayName_call = function(){
+    console.log(this.name);
+}
+
+let sayName_apply = function(toys1, toys2){
+    console.log(this.name + ' likes ' + toys1 + ' and ' + toys2);
+}
+
+var me = {
+    name: 'Jellington',
+    age: 80
+}
+
+let toys = ['ball', 'chew']
+
+
+sayName_call.call(me);
+sayName_apply.apply(me, toys);
+let newFunc = sayName_apply.bind(me, toys[0], toys[1]);
+newFunc();
+
+/*
+New binding
+*/
+
+var Dog = function(breed){
+    this.breed = breed;
+}
+let chihuahua = new Dog('Chihuahua')
+
+/*
+Window binding - catch all case
+*/
+window.name = 'Sunny';
+
+
+let sayName_windowbinding = function(){
+    console.log(this.name);
+}
+
+
+sayName_windowbinding();
+
