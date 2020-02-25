796033a9fcc2a5e5b9fd15fce41768fe1a8270b8
Jasmine <jasmine.kim.18@gmail.com>
2020-02-25 12:32:30 -0800

    Flask and React are integrated

---
+++ react/building_blocks/flask-backend/static/react/precache-manifest.e4e2f28424fcaa226597f4cfa63670e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc416082b9d9ce8c446948fa416f5c0",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "4a588df8b0b8fe1af253",
+    "url": "/static/react/js/2.1e702bab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/react/js/2.1e702bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "193838c5ec2f8d9127fd",
+    "url": "/static/react/js/main.1c7875ab.chunk.js"
+  },
+  {
+    "revision": "c7c6535e0008bb76ce43",
+    "url": "/static/react/js/runtime-main.621c8714.js"
+  }
+]);
